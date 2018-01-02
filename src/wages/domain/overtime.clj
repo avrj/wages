13458bc0de3df8bf52fe6fd4fe45b2982fff89f9
Anton Räihä <anraiha@cs.helsinki.fi>
2018-01-02 15:04:30 +0200

    Fix overtime calculation logic

--- src/wages/domain/overtime.clj
+++ src/wages/domain/overtime.clj
@@ -1,48 +1,17 @@
 (ns wages.domain.overtime
-    (:require [clj-time.core :as t]
-              [wages.rates.rate :refer :all]
-              [wages.util.date :refer :all]
-              [wages.domain.workshift :refer :all]))
+    (:require [wages.rates.rate :refer :all]))
 
-(defn overtime-wage [overtime-compensation-rate]
-  (let [overtime-compensation-percent (get overtime-compensation-rate :overtime-compensation-percent)]
-    (* hourly-wage overtime-compensation-percent)))
-  
-(defn overtime-date [overtime-hours workshift-start-date]
-    "Returns the date of overtime start/end"
-  (let [full-hours     (+ regular-working-hours overtime-hours)
-        hours          (t/hours full-hours)]
-    (t/plus workshift-start-date hours)))
+(defn- overtime-calculation [overtime-compensation overtime-hours]
+ (if (>= overtime-hours (+ regular-working-hours (get overtime-compensation :overtime-compensation-start)))
+    (if (> (+ regular-working-hours (get overtime-compensation :overtime-compensation-end)) overtime-hours)
+        (* hourly-wage (get overtime-compensation :overtime-compensation-percent) (- (- overtime-hours 8) (get overtime-compensation :overtime-compensation-start)))
+        (* hourly-wage (get overtime-compensation :overtime-compensation-percent) (- (get overtime-compensation :overtime-compensation-end) (get overtime-compensation :overtime-compensation-start))))
+    0))
 
-(defn- overtime-start-time [overtime-compensation-rate workshift-start-date]
-  (overtime-date (get overtime-compensation-rate :overtime-compensation-start) workshift-start-date))
-  
-(defn- overtime-end-time [overtime-compensation-rate workshift-start-date]
-  (overtime-date (get overtime-compensation-rate :overtime-compensation-end) workshift-start-date))
-
-(defn overlap-between-shift-and-overtime [workshift-start-date workshift-end-date overtime-compensation-rate]
-    (let [calculated-overtime-start-time (overtime-start-time overtime-compensation-rate workshift-start-date)
-          calculated-overtime-end-time   (overtime-end-time overtime-compensation-rate workshift-start-date)
-          workshift-interval (t/interval workshift-start-date workshift-end-date)
-          overtime-interval (t/interval calculated-overtime-start-time calculated-overtime-end-time)]
-    (t/overlap workshift-interval overtime-interval)))
-
-(defn overtime? [working-hours]
+(defn- overtime? [working-hours]
     (> working-hours regular-working-hours))
 
-(defn overtime-hours [workshift-start-date workshift-end-date overtime-compensation-rate]
-    (let [calculated-shift-duration-in-hours (shift-duration-in-hours workshift-start-date workshift-end-date)]
-        (if (overtime? calculated-shift-duration-in-hours)
-        (if (nil? (overlap-between-shift-and-overtime workshift-start-date workshift-end-date overtime-compensation-rate))
-            0
-            (interval->hours (overlap-between-shift-and-overtime workshift-start-date workshift-end-date overtime-compensation-rate)))
-        0)))
-
-(defn calculate-single-overtime-wage [workshift-start-date workshift-end-date overtime-compensation-rate] 
-    (calculate-period-wage (overtime-hours workshift-start-date workshift-end-date overtime-compensation-rate) (overtime-wage overtime-compensation-rate)))
-
-(defn calculate-overtime-wage [workshift-start-date workshift-end-date]
-    (bigdec (apply + (map #(calculate-single-overtime-wage workshift-start-date workshift-end-date %) overtime-compensation-rates))))
-
 (defn calculate-overtime-wage-based-on-overtime-hours [overtime-hours]
-    (bigdec (apply + (map (fn [overtime-compensation] (if (<= (+ regular-working-hours (get overtime-compensation :overtime-compensation-start)) overtime-hours) (* hourly-wage (get overtime-compensation :overtime-compensation-percent) (- (if (> (get overtime-compensation :overtime-compensation-end) overtime-hours) (- overtime-hours regular-working-hours) (get overtime-compensation :overtime-compensation-end)) (get overtime-compensation :overtime-compensation-start))) 0) ) overtime-compensation-rates))))
+    (if (overtime? overtime-hours)
+        (bigdec (apply + (map (fn [overtime-compensation] (overtime-calculation overtime-compensation overtime-hours)) overtime-compensation-rates)))
+        0M))
