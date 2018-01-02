c58e51f98a5dc3066e7e4332051c49ef11a22e90
Anton Räihä <anraiha@cs.helsinki.fi>
2018-01-02 12:20:55 +0200

    Fix transformations for overtime calculations

--- src/wages/data/transformation.clj
+++ src/wages/data/transformation.clj
@@ -3,7 +3,8 @@
               [wages.util.date :refer :all]
               [wages.util.currency :refer :all]
               [wages.domain.wage :refer :all]
-              [wages.domain.workshift :refer :all]))
+              [wages.domain.workshift :refer :all]
+              [wages.domain.overtime :refer :all]))
     
 (defn replace-time-from-workshift [workshift key date]
     (update workshift key #(replace-time-in-date % date)))
@@ -18,15 +19,28 @@
             total-daily-pay (calculate-total-daily-pay start end)]
         (assoc workshift :total-daily-pay total-daily-pay)))
 
-(defn construct-monthly-wage-map-from-workshift [[id workshift]]
-    {:id    id
+(defn construct-monthly-wage-map-from-workshift [workshift]
+    (let [monthly-wage (apply + (map :monthly-wage workshift))]
+        {:id (get (first workshift) :id) 
+        :name  (get (first workshift) :name) 
+        :monthly-wage monthly-wage}))
+
+(defn overtime-for-day [[_ workshift]]
+    (let [monthly-wage (round-to-nearest-cents (apply + (map :total-daily-pay workshift)))
+          daily-overtime-wage (calculate-overtime-wage-based-on-overtime-hours (apply + (map (fn [x] (shift-duration-in-hours (get x :Start) (get x :End))) workshift)))]
+    {:id (get (first workshift) (keyword "Person ID")) 
      :name  (get (first workshift) (keyword "Person Name")) 
-     :monthly-wage (round-to-nearest-cents (apply + (map :total-daily-pay workshift)))})
+     :monthly-wage (+ monthly-wage daily-overtime-wage)}))
+
+(defn overtime-x [workshifts]
+    (map (fn [[id workshift]] (mapv overtime-for-day (group-by (fn [workshift] (t/date-time (t/year (get workshift :Start)) (t/month (get workshift :Start)) (t/day (get workshift :Start)))) workshift))) workshifts))
 
 (defn transform-to-monthly-wages [workshifts]
     (->> workshifts
         (group-by (keyword "Person ID")) 
+        (overtime-x)
         (map construct-monthly-wage-map-from-workshift)))
+
 
 (defn parse-time-from-workshift [workshift key]
     (update workshift key #(parse-time %)))
